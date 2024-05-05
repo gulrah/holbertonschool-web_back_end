61765efed945b1d55d01f3c6a428a96c68ca7bc1
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-05 06:05:41 -0700

    fix

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 """
-Let's execute multiple coroutines at the same time with async
+Tasks
 """
-from typing import List
 import asyncio
+from typing import List
+from asyncio import Task
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Asynchronous routine that spawns wait_random n times
+    Asynchronous function that spawns wait_random n times
     """
-    delays = [wait_random(max_delay) for _ in range(n)]
-    completed_delays = await asyncio.gather(*delays)
-    sorted_delays = sorted(completed_delays)
-    return sorted_delays
+    tasks: List[Task] = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]

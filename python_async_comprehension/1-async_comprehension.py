ae2ebbdac6f2c24922aace9814f09d94662740ae
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-05 06:50:15 -0700

    Update 1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,28 +2,14 @@
 """
 Async Comprehensions
 """
-from typing import List
-from random import uniform
 import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
-async def async_generator() -> float:
-    """
-    Generates 10 random floats between 0 and 10 with 1 second delay.
-    """
-    for _ in range(10):
-        await asyncio.sleep(1)
-        yield uniform(0, 10)
 
 async def async_comprehension() -> List[float]:
     """
-    Collects 10 random floats using async comprehension.
+    async_comprehension function
     """
-    result = [number async for number in async_generator()]
-    return all(isinstance(number, float) for number in result), all(0 <= number <= 10 for number in result)
-
-# For testing purposes
-async def main():
-    results = await async_comprehension()
-    print(results)
-
-asyncio.run(main())
+    valuesList = [i async for i in async_generator()]
+    return valuesList

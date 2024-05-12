227d11cc0c2da06069b7cbc9535ecdfbefb32912
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-12 11:53:16 -0700

    Update 12-createReportObject.js

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,12 +1,10 @@
-import createEmployeesObject from './11-createEmployeesObject.js';
-
 export default function createReportObject(employeesList) {
-    return {
-	allEmployees: {
-	    ...employeesList,
-	},
-	getNumberOfDepartments() {
-	    return Object.keys(this.allEmployees).length;
-	},
-    };
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
 }

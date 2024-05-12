79cda9f2545e62c2984670b37cf0844bb0302fbd
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-12 07:21:23 -0700

    Updates

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -3,44 +3,55 @@
 Deletion-resilient hypermedia pagination
 """
 
+import csv
+import math
 from typing import List, Dict
-from .1-simple_pagination import Server
 
 
-class Server(Server):
+class Server:
+    """Server class to paginate a database of popular baby names.
     """
-    Server class to paginate a database of popular baby names.
-    """
-    
+    DATA_FILE = "Popular_Baby_Names.csv"
+
     def __init__(self):
-        super().__init__()
+        self.__dataset = None
         self.__indexed_dataset = None
-        
-    def indexed_dataset(self) -> Dict[int, List]:
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
         """
-        Dataset indexed by sorting position, starting at 0
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
                 i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
-        
+
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """Return deletion-resilient hypermedia pagination."""
-        assert index is None or 0 <= index < len(self.dataset()), "Index out of range"
-        data = []
-        next_index = None
-        
-        if index is not None:
-            data = [self.indexed_dataset()[i] for i in range(index, min(index + page_size, len(self.dataset())))]
-            next_index = min(index + page_size, len(self.dataset()))
-            
-        hyper_dict = {
-            "index": index if index is not None else 0,
-            "data": data,
-            "page_size": len(data),
-            "next_index": next_index
-        }
-        return hyper_dict
+        """ return a dictionary """
+        assert type(index) == int and type(page_size) == int
+        assert 0 <= index < len(self.indexed_dataset())
+        pages = []
+        next_index = index + page_size
+        for i in range(index, index + page_size):
+            if not self.indexed_dataset().get(i):
+                i += 1
+                next_index += 1
+            pages.append(self.indexed_dataset()[i])
+        return {'index': index,
+                'next_index': next_index,
+                'page_size': page_size,
+                'data': pages
+                }

647d69aec0c57d723935788d2119a7fe05330d47
gulrah <gulnarrahimli@mail.ru>
2024-05-14 08:14:37 +0000

    First Commit

---
+++ ES6_classes/9-hoisting.js
@@ -0,0 +1,23 @@
+import HolbertonClass from './8-hbtn_class.js';
+
+export class StudentHolberton {
+    constructor(firstName, lastName, holbertonClass) {
+	this._firstName = firstName;
+	this._lastName = lastName;
+	this._holbertonClass = holbertonClass;
+    }
+
+    get fullName() {
+	return `${this._firstName} ${this._lastName}`;
+    }
+
+    get holbertonClass() {
+	return this._holbertonClass;
+    }
+
+    get fullStudentDescription() {
+	return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    }
+}
+
+export const listOfStudents = [];

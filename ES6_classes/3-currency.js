b743efc3b14192370bf8fadd18387b5a7616ff2b
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-18 20:25:50 +0400

    Update 3-currency.js

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,33 +1,26 @@
-// 4-pricing.js
-import Currency from './3-currency';
-
-export default class Pricing {
-  constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
   }
 
-  get amount() {
-    return this._amount;
+  get code() {
+    return this._code;
   }
 
-  set amount(value) {
-    this._amount = value;
+  set code(value) {
+    this._code = value;
   }
 
-  get currency() {
-    return this._currency;
+  get name() {
+    return this._name;
   }
 
-  set currency(value) {
-    this._currency = value;
+  set name(value) {
+    this._name = value;
   }
 
-  displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-  }
-
-  static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
   }
 }

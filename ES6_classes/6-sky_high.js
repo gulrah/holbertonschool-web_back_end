1c15d7533159a484d5e6e02fd4a6d35544947577
Gulnar Rahimli <146723639+gulrah@users.noreply.github.com>
2024-05-19 12:28:38 +0400

    Update 6-sky_high.js

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,7 +1,13 @@
+import Building from './5-building';
+
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
+  }
+
+  get sqft() {
+    return this._sqft;
   }
 
   get floors() {
@@ -9,6 +15,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }

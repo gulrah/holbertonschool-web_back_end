259e20459369a21cce00bee44e4ccb004560c02e
gulrah <gulnarrahimli@mail.ru>
2024-05-20 08:00:01 +0000

    Added whitespaces

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -7,7 +7,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   return Promise.allSettled([userPromise, photoPromise])
     .then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
   })));
 }

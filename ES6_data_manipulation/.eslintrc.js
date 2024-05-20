107f547b73e53f044b2c5085b252c5c555cc53c1
gulrah <gulnarrahimli@mail.ru>
2024-05-20 08:24:51 +0000

    Packange Json

---
+++ ES6_data_manipulation/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+      es6: true,
+      node: true,
+      jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
